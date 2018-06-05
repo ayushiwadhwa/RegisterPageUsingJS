9cef8b4efefb4aee81d5f4ead45637d1fe90aeb7
Ayushi <awadhwa01.ayu@gmail.com>
2018-06-05 11:12:05 -0400

    Completed validations for all the fields. Added localstorage.

--- Register.js
+++ Register.js
@@ -1,7 +1,7 @@
 document.getElementById('btn').addEventListener('click', function(){
     
     var err_elems = document.querySelectorAll('div[id^="div_err"]');
-    var len = err_elems.length;
+    var check = false;
     
     if(err_elems.length !== 0){
         for(var i=0; i<len; i++) {
@@ -9,31 +9,74 @@ document.getElementById('btn').addEventListener('click', function(){
         }
     }
     
-    var uname = document.getElementById('uname').innerHTML;
-    console.log(uname.length);
-    if(uname){
+    var uname = document.getElementById('uname').value;
+    if(uname === ""){
+            check = false;
             document.getElementById('uname').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please enter a valid username</div>');
     }else{
-        console.log('Hi');
+        check = true;
     }
      
-    var pswd = document.getElementById('pwd').innerHTML;
-    if(pswd.length === 0){
+    var pswd = document.getElementById('pwd').value;
+    if(pswd === ""){
+        check = false;
         document.getElementById('pwd').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please enter a valid password</div>');
+    }else{
+        check = true;
     }
     
-    var email = document.getElementById('email').innerHTML;
-    if(email.length === 0){
+    var email = document.getElementById('email').value;
+    if(email === ""){
+        check = false;
         document.getElementById('email').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please enter a valid email id</div>');
+    }else{
+        check = true;
     }
     
-    var loc = document.getElementById('loc').innerHTML;
-    if(loc.length === 0){
+    var loc = document.getElementById('loc').value;
+    if(loc === ""){
+        check = false;
         document.getElementById('loc').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please enter a valid location</div>');
+    }else{
+        check = true;
     }
     
-    var comp = document.getElementById('comp').innerHTML;
-    if(comp.length === 0){
+    var comp = document.getElementById('comp').value;
+    if(comp === ""){
+        check = false;
         document.getElementById('comp').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please enter a valid company</div>');
+    }else{
+        check = true;
+    }
+    
+    if(!(document.getElementById('rdMale').checked) && !(document.getElementById('rdFemale').checked)) {
+        check = false;
+        document.getElementById('hid').insertAdjacentHTML('afterend','<div id="div_err" style="color: red;"}>Please select a gender</div>');
+    }else{
+        check = true;
+        var gender;
+        if (document.getElementById('rdMale').checked) {
+           gender = document.getElementById('rdMale').value;
+        }else{
+            gender = document.getElementById('rdFemale').value;
+        }
+    }
+    
+    if(check){
+            var users = [];
+            if(localStorage.a_users) {
+                users = JSON.parse(localStorage.a_users);
+                //displayUsers(users);
+            }
+            var user = {
+                'username':uname,
+                'password':pswd,
+                'email':email,
+                'location':loc,
+                'company':comp,
+                'gender':gender
+            };
+            users.push(user);
+            localStorage.a_users = JSON.stringify(users);
     }
 })
